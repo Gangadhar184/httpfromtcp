c4dbb0c0948eaa255e49438e55ce1033e6ef347a
Gangadhar184 <zenitsuig65@gmail.com>
2026-06-03 19:44:36 +0530

    read 8 bytes from the file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,59 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.io.FileInputStream;
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+/**
+ *  your program will now read messages.txt 8 bytes at a time
+ *  and print that data back to stdout in 8 byte chunks.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //open the file txt for reading
+        //FileInputStream so we can read bytes from a file.
+        try(FileInputStream fs = new FileInputStream("message.txt")) {
+            //creating a byte array for capacity of 8 bytes[?,?,?,?,?,?,?,?]
+            byte[] buffer = new byte[8];
+            //keep reading until end of file
+            while(true) {
+                //read up to 8 bytes form the file into buffer,
+                //eg: if txt file contains "helloworld"
+                //first read: buffer = [h,e,l,l,o,w,o,r] bytreads = 8
+                //second read: buffer = [l,d,o,w,o,r,...], byteread = 2
+                // only first 2bytes are new data
+                int byteReads = fs.read(buffer);
+                if(byteReads == -1) break; //if its -1 we are end of file
+                //convert only the bytes that were actually read into string,
+                // Parameters:
+                // buffer      -> source byte array
+                // 0           -> start at index 0
+                // bytesRead   -> number of valid bytes
+                //
+                // Why not:
+                // new String(buffer)
+                //
+                // Because the last read might not fill the buffer.
+                //
+                // Example:
+                // buffer size = 8
+                //
+                // Previous read:
+                // "abcdefgh"
+                //
+                // Last read:
+                // only "xy"
+                //
+                // buffer becomes:
+                // [x, y, c, d, e, f, g, h]
+                //
+                // new String(buffer)
+                // -> "xycdefgh" (WRONG)
+                //
+                // new String(buffer, 0, 2)
+                // -> "xy" (CORRECT)
+                String chunk = new String(buffer,0, byteReads);
+                System.out.printf("read: %s%n", chunk);
+            }
         }
     }
 }
